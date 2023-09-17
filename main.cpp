61e1a5e58ad188c56ba247d206b6c9c7d075985f
HonfiBotond <hobot2002@gmail.com>
2023-09-17 20:12:21 +0200

    Hibák kijavítva

--- main.cpp
+++ main.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 
-constexpr int N_ELEMENTS = 100;
+int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //NELEMENTS helyett N_ELEMENTS
-    std::cout << '1-100 ertekek duplazasa' //Hiányzó pontosvessző
-    for (int i = 0;) //Rossz syntax
+    int *b = new int[N_ELEMENTS]; 
+    std::cout << '1-100 ertekek duplazasa'; 
+    for (int i = 0; i < N_ELEMENTS; i++) 
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:" //pontosvessző
+        std::cout << "Ertek:"; 
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++) // pontosvessző
-    {
-        atlag += b[i] // pontosvessző
+    for (int i = 0; i < N_ELEMENTS; i++){
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
