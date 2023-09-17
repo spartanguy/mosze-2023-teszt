e94d3550fb24caeb0c31fc730941faa568b7dd10
HonfiBotond <hobot2002@gmail.com>
2023-09-17 20:04:51 +0200

    Hibák kommentelése

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //NELEMENTS helyett N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa' //Hiányzó pontosvessző
+    for (int i = 0;) //Rossz syntax
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //pontosvessző
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++) // pontosvessző
     {
-        atlag += b[i]
+        atlag += b[i] // pontosvessző
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
